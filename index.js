b8a2b18f6582161630ae06faf1c624ee5c5ad769
ahmed rashid <ahmeddmrashid@gmail.com>
2022-09-26 11:49:53 +0300

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function (num1, num2){
+return num1 / num2 
+}
+divide (2000, 100)
+
+
+const square = (num1) => num1 * num1;
+square(5)
+
+const add = (num1, num2) => num1 + num2;
+add(2, 4)
